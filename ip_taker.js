70638406d7ac1c99b6c69c92be489c277dd5a70e
Creater2 <105660444+Creater2@users.noreply.github.com>
2022-08-20 00:42:09 +0300

    Update ip_taker.js

--- ip_taker.js
+++ ip_taker.js
@@ -1,20 +1,21 @@
-
-   $.get("http://ipinfo.io", function(response) {
-    console.log(response);
+$.getJSON("https://api.ipify.org/?format=json", function(e) {
+    console.log(e.ip);
     var d = new Date();
     var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     console.log(time);
 
     let input_for_ip = document.querySelector('.input_for_ip')
-    input_for_ip.value = ('ip адресс: ' + response.ip + '  Время перехода: ' + time);
+    input_for_ip.value = ('ip адресс: ' + e.ip + '  Время перехода: ' + time);
     let submit_activation = document.querySelector('.ip_form').submit()
     
 
 
-    console.log('ip адресс:' + response.ip);
+    console.log('ip адресс:' + e.ip);
+
+});
 
 
-}, "jsonp");
+ 
 
 
 
